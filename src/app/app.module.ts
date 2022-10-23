45cfa01c7cebbb6c96de088bf65cd372efcb5171
em <pattybucala129@gmail.com>
2022-10-23 23:00:44 +0800

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
+import { ChooseOrderComponent } from './choose-order/choose-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerNavbarComponent,
+    ChooseOrderComponent
   ],
   imports: [
     BrowserModule,
