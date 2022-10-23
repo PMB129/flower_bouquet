6aea65f96df91232d27c5546cc5984f0f03441af
em <pattybucala129@gmail.com>
2022-10-24 00:12:36 +0800

    buy and customize button in choose_order module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChooseOrderComponent } from './choose-order/choose-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:ChooseOrderComponent,
+    path:'choose_order',
+    title: 'Order'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
