0c5c00c46fcc1af0b1af1d3339f0672a79687ac8
em <pattybucala129@gmail.com>
2022-10-24 23:32:53 +0800

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChooseOrderComponent } from './choose-order/choose-order.component';
 import { OrderArrangedComponent } from './order-arranged/order-arranged.component';
+import { OrderCustomizePage1Component } from './order-customize-page1/order-customize-page1.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,12 @@ const routes: Routes = [
   {
     component:OrderArrangedComponent,
     path:'order_arranged',
-    title:'Order || Buy'
+    title:'Order || Buy Bouquet'
+  },
+  {
+    component:OrderCustomizePage1Component,
+    path:'order_customize_page1',
+    title:'Order || Customize Bouquet'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 import { ChooseOrderComponent } from './choose-order/choose-order.component';
 import { OrderArrangedComponent } from './order-arranged/order-arranged.component';
+import { OrderCustomizePage1Component } from './order-customize-page1/order-customize-page1.component';
+import { OrderCustomizeFlowersComponent } from './order-customize-flowers/order-customize-flowers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerNavbarComponent,
     ChooseOrderComponent,
-    OrderArrangedComponent
+    OrderArrangedComponent,
+    OrderCustomizePage1Component,
+    OrderCustomizeFlowersComponent
   ],
   imports: [
     BrowserModule,
