35a20e63cbb5ce55f86341c9a1c238d7001cdecc
jedpogi54321 <jedvtan34@gmail.com>
2022-10-24 12:36:33 +0800

    Admin Sales and Admin Sales (View Details) UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { SalesComponent } from './sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:SalesComponent,
+    path:'sales'
+  },
+
+  {
+    component:OrderDetailsComponent,
+    path:'order-details'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SalesComponent } from './sales/sales.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    SalesComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
